e2933a19830e39df399d9b419aa8803771a8b9c8
Justmaina <githuamaina10@gmail.com>
2025-07-27 15:03:33 +0300

    the newest

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import './app.css'
 
 function App() {
   const [memeProperties, setMemeProperties] = useState({
@@ -7,18 +8,34 @@ function App() {
     memeImage: ""
   })
 
+  const [allMemes, setAllMemes] = useState([])
+
   useEffect (() => {
     async function getMemes() {
       try {
-        const res = await fetch(' https://api.imgflip.com/get_memes');
+        const res = await fetch('https://api.imgflip.com/get_memes');
         const data = await res.json();
-        setMemeProperties(data)
+        setAllMemes(data.data.memes)
+        setMemeProperties (prev => ({
+          ...prev,
+          memeImage: data.data.memes[0].url
+        }))
     }catch(error) {
-      console.log("Could fetch the memes")
+      console.log("Couldn't fetch the memes")
     }
   }
-}
-)
+  getMemes();
+}, [])
+
+  function getRandomImage () {
+    const randomIndex = Math.floor(Math.random() * allMemes.length)
+    const randomMeme = allMemes[randomIndex]
+    setMemeProperties (prev => ({
+      ...prev,
+      memeImage: randomMeme.url
+    }))
+  }
+
 
   function updateText (event) {
     const {name, value} = event.target
@@ -30,23 +47,33 @@ function App() {
 
 
   return (
-   <div>
+   <div className='container'>
     <input 
       type='text'
       placeholder='Text #1'
-      name='TextOne'
-    >{updateText}</input>
+      name='textOne'
+      value={memeProperties.textOne}
+      onChange={updateText}
+      className="meme-input"
+
+    ></input>
     <input
       type='text'
       placeholder='Text #2'
-      name='TextTwo'
-    >{updateText}</input>
-    <img
-      src= {memeImage}
-      alt='memeImage'
-    ></img>
-    <button>Get a new Image</button>
-    <button>Share</button>
+      name='textTwo'
+      value={memeProperties.textTwo}
+      onChange={updateText}
+      className="meme-input"
+    ></input>
+    {memeProperties.memeImage && (
+    <div className="meme-container">
+      <img src={memeProperties.memeImage} alt="Meme" className="meme-img" />
+      <h2 className="meme-text top">{memeProperties.textOne}</h2>
+      <h2 className="meme-text bottom">{memeProperties.textTwo}</h2>
+    </div>
+  )}
+    <button onClick = {getRandomImage} className='meme-button'>Get a new Image</button>
+    <button className='meme-button' >Share</button>
    </div>
   )
 }
